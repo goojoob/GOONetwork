741d4272fb96ed97738e4b7dc7adbc3c08d166eb
Antonio Rodríguez <antrorc@gmail.com>
2022-12-06 16:06:07 +0100

    Test Units

--- Sources/GOONetwork/GOONetwork.swift
+++ Sources/GOONetwork/GOONetwork.swift
@@ -1,6 +1,43 @@
-public struct GOONetwork {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+@available(macOS 12.0, *)
+@available(iOS 13.0, *)
+public struct GOONetwork {
+
+    static public func decodeJSON<T: Decodable>(fromUrl: String) async throws -> T? {
+        var data: Data = Data()
+        var response: URLResponse = URLResponse()
+
+        guard let url = URL(string: fromUrl) else {
+            throw NetworkError.urlError
+        }
+
+        do {
+            let urlRequest = URLRequest(url: url)
+            (data, response) = try await URLSession.shared.data(from: urlRequest.url!)
+
+            guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+                throw NetworkError.responseError
+            }
+        } catch let err as NetworkError {
+            throw err
+        } catch {
+            throw NetworkError.dataError
+        }
+
+        do {
+            let decodedJSON = try JSONDecoder().decode(T.self, from: data)
+            return decodedJSON
+        } catch {
+            throw NetworkError.decodeError
+        }
     }
+
+}
+
+enum NetworkError: String, Error {
+    case urlError
+    case responseError
+    case dataError
+    case decodeError
 }
--- Tests/GOONetworkTests/GOONetworkTests.swift
+++ Tests/GOONetworkTests/GOONetworkTests.swift
@@ -1,11 +1,95 @@
 import XCTest
 @testable import GOONetwork
 
+@available(macOS 12.0, *)
+@available(iOS 13.0, *)
 final class GOONetworkTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(GOONetwork().text, "Hello, World!")
+
+    //Defines a Product struct that conforms to the Decodable protocol, which means it can be initialized from a JSON object.
+    //Creates an expectation, which is a way to specify that some operation is expected to happen within a certain time frame.
+    //Sets the URL to a valid one that returns a JSON object.
+    //Tries to decode the JSON object into a Product instance using the GOONetwork.decodeJSON(fromUrl:) method.
+    //If the Product instance is valid, it checks that its title and description properties have the expected values, and then it fulfills the expectation.
+    //If the Product instance is not valid, it fails the test with a message indicating that it failed to decode the JSON.
+    //If an error occurred while trying to decode the JSON, it also fails the test with a message indicating that it failed to decode the JSON.
+    //Waits for the expectation to be fulfilled, and if it isn't fulfilled within 10 seconds, it fails the test with a timeout error.
+    func testDecodeJSONWithValidURL() async {
+        struct Product: Decodable {
+            let title: String
+            let description: String
+        }
+
+        let expectation = XCTestExpectation(description: "Decode JSON from URL")
+        let url = "https://dummyjson.com/products/1"
+
+        do {
+            let product: Product? = try await GOONetwork.decodeJSON(fromUrl: url)
+
+            if let product {
+                XCTAssertEqual(product.title, "iPhone 9")
+                XCTAssertEqual(product.description, "An apple mobile which is nothing like apple")
+                expectation.fulfill()
+            } else {
+                XCTFail("Failed to decode JSON from url \(url)")
+            }
+        } catch {
+            XCTFail("Failed to decode JSON from url \(url)")
+        }
+
+        wait(for: [expectation], timeout: 10.0)
+    }
+
+    //Here, we're creating a test expectation, setting the URL to an invalid one, and then trying to decode the JSON.
+    //If we get a valid Product instance, we fail the test because we expected to fail to decode the JSON. Otherwise, we fulfill the expectation.
+    //Finally, we wait for the expectation to be fulfilled.
+    func testDecodeJSONWithInvalidURL() async {
+        struct Product: Decodable {
+            let title: String
+            let description: String
+        }
+
+        let expectation = XCTestExpectation(description: "Decode JSON from invalid URL")
+        let url = "https://dummyjson.com/invalid-url"
+
+        do {
+            let product: Product? = try await GOONetwork.decodeJSON(fromUrl: url)
+
+            if product != nil {
+                XCTFail("Expected to fail to decode JSON from url \(url)")
+            } else {
+                expectation.fulfill()
+            }
+        } catch {
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 10.0)
+    }
+
+    //Here, we're doing the same thing as before, but instead of using an invalid URL, we're using a valid URL but expecting the JSON it returns to be invalid.
+    //If we get a valid Product instance, we fail the test because we expected to fail to decode the JSON.
+    //Otherwise, we fulfill the expectation. Finally, we wait for the expectation to be fulfilled.
+    func testDecodeInvalidJSON() async {
+        struct Product: Decodable {
+            let invalidTitle: String
+            let invalidDesc: String
+        }
+
+        let expectation = XCTestExpectation(description: "Decode JSON from invalid JSON")
+        let url = "https://dummyjson.com/products/1"
+
+        do {
+            let product: Product? = try await GOONetwork.decodeJSON(fromUrl: url)
+
+            if product != nil {
+                XCTFail("Expected to fail to decode JSON from url \(url)")
+            } else {
+                expectation.fulfill()
+            }
+        } catch {
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 10.0)
     }
 }
